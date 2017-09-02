8809fbb7868d62b7ae695028a234de6e986e3d13
Rahul Singh <rkant225@gmail.com>
2017-09-02 18:24:15 +0000

    Second commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
+ belongs_to :user
+ has_many :comments, dependent: :destroy
+ validates_presence_of :title
+ validates_presence_of :body
+ 
 end
